c1a3db2488fd81deca58b86620e31daffd0b6330
allden <30358515+allden@users.noreply.github.com>
2020-06-17 21:52:37 +0300

    Added android keyboard fix, hopefully.

---
+++ public/scripts/privateChatFunctionality/androidFocusFix.js
@@ -0,0 +1,12 @@
+document.body.addEventListener("focus", event => {
+    const target = event.target;
+    switch (target.tagName) {
+        case "INPUT":
+        case "TEXTAREA":
+        case "SELECT":
+            document.body.classList.add("keyboard");
+    }
+}, true); 
+document.body.addEventListener("blur", () => {
+    document.body.classList.remove("keyboard");
+}, true); 
